073637e10bb9d0249f7fed136f31b20c6730c41c
Muhammad Hasnain Ahmed <ahmedhasnain590@gmail.com>
2022-06-10 00:26:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var modalImg = document.getElementById("modalImg");
+var modalProductName = document.getElementById("modalProductName");
+var modalProductDetails = document.getElementById("modalProductDetails");
+
+function openModal(img, prodName) {
+    console.log("helloo");
+    modalImg.src = img;
+    modalProductName.innerHTML = prodName;;
+}
+var modalImg = document.getElementById("modalImg");
+var modalProductName = document.getElementById("modalProductName");
+var modalProductDetails = document.getElementById("modalProductDetails");
+
+function openModal(img, prodName) {
+    console.log("helloo");
+    modalImg.src = img;
+    modalProductName.innerHTML = prodName;
+}
